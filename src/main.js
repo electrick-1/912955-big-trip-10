4d9baba5ca29031dd8f5d2961c5b6df6a1f88316
electrick-1 <elbandido93@mail.ru>
2020-01-06 19:15:08 +0700

    оптимизация кода

--- src/main.js
+++ src/main.js
@@ -24,54 +24,52 @@ render(tripControls, new ControlsComponent().getElement(), RenderPosition.BEFORE
 render(tripControls, new FiltersComponent(filters).getElement(), RenderPosition.BEFOREEND);
 render(tripEvents, new EventContainerComponent().getElement(), RenderPosition.BEFOREEND);
 
-const tripDays = document.querySelector(`.trip-days`);
-const dates = [
-  ...new Set(cards.map((card) => new Date(card.startDate).toDateString()))
-];
-
-dates.forEach((day, index) => {
-  render(tripDays, new DayComponent(day, index).getElement(), RenderPosition.BEFOREEND);
-  const dayContainer = tripDays.querySelector(`.trip-days__item:last-of-type`);
-  cards.filter((event) => day === new Date(event.startDate).toDateString())
-    .forEach((event) => {
-      const eventList = dayContainer.querySelector(`.trip-events__list`);
-
-      const onEscKeyDown = (evt) => {
-        const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-        if (isEscKey) {
-          replaceEditToEvent();
-          document.removeEventListener(`keydown`, onEscKeyDown);
-        }
-      };
-
-      const replaceEditToEvent = () => {
-        eventList.replaceChild(eventComponent.getElement(), editEventComponent.getElement());
-      };
-
-      const replaceEventToEdit = () => {
-        eventList.replaceChild(editEventComponent.getElement(), eventComponent.getElement());
-      };
-
-      const eventComponent = new EventsComponent(event);
-      const editButton = eventComponent.getElement().querySelector(`.event__rollup-btn`);
-      editButton.addEventListener(`click`, () => {
-        replaceEventToEdit();
-        document.addEventListener(`keydown`, onEscKeyDown);
-      });
-
-      const editEventComponent = new EditEventsComponent(event);
-      editEventComponent.getElement().addEventListener(`submit`, () => replaceEditToEvent);
-
-      render(eventList, eventComponent.getElement(), RenderPosition.BEFOREEND);
-    }
-
-    );
-});
-
 if (cards.length === 0) {
   addEventButton.setAttribute(`disabled`, true);
   render(tripEvents, new NoEventsComponent().getElement(), RenderPosition.BEFOREEND);
 } else {
   render(tripInfo, new TripInfoComponent(cards).getElement(), RenderPosition.AFTERBEGIN);
-  render(tripEvents, new SortComponent(sortOptions).getElement(), RenderPosition.BEFOREEND);
+  render(tripEvents, new SortComponent(sortOptions).getElement(), RenderPosition.AFTERBEGIN);
+
+  const tripDays = document.querySelector(`.trip-days`);
+  const dates = [
+    ...new Set(cards.map((card) => new Date(card.startDate).toDateString()))
+  ];
+
+  dates.forEach((day, index) => {
+    render(tripDays, new DayComponent(day, index).getElement(), RenderPosition.BEFOREEND);
+    const dayContainer = tripDays.querySelector(`.trip-days__item:last-of-type`);
+    cards.filter((event) => day === new Date(event.startDate).toDateString())
+      .forEach((event) => {
+        const eventList = dayContainer.querySelector(`.trip-events__list`);
+
+        const onEscKeyDown = (evt) => {
+          const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+          if (isEscKey) {
+            replaceEditToEvent();
+            document.removeEventListener(`keydown`, onEscKeyDown);
+          }
+        };
+
+        const replaceEditToEvent = () => {
+          eventList.replaceChild(eventComponent.getElement(), editEventComponent.getElement());
+        };
+
+        const replaceEventToEdit = () => {
+          eventList.replaceChild(editEventComponent.getElement(), eventComponent.getElement());
+        };
+
+        const eventComponent = new EventsComponent(event);
+        const editButton = eventComponent.getElement().querySelector(`.event__rollup-btn`);
+        editButton.addEventListener(`click`, () => {
+          replaceEventToEdit();
+          document.addEventListener(`keydown`, onEscKeyDown);
+        });
+
+        const editEventComponent = new EditEventsComponent(event);
+        editEventComponent.getElement().addEventListener(`submit`, () => replaceEditToEvent);
+
+        render(eventList, eventComponent.getElement(), RenderPosition.BEFOREEND);
+      });
+  });
 }
