781edae6b95bb167fb7c6ba6e893027d9f96a46d
electrick-1 <elbandido93@mail.ru>
2020-01-06 18:58:51 +0700

    При отсутствии точек маршрута теперь выводится сообщение

--- src/main.js
+++ src/main.js
@@ -7,22 +7,21 @@ import SortComponent from './components/sort.js';
 import EventsComponent from './components/event-item.js';
 import EditEventsComponent from './components/edit-event.js';
 import EventContainerComponent from './components/event-list.js';
+import NoEventsComponent from './components/no-events.js';
 import DayComponent from './components/day.js';
 import {cards} from './mock/events.js';
 import {sortOptions} from './mock/sort.js';
 import {filters} from './mock/filters.js';
 
-
 const tripMain = document.querySelector(`.trip-main`);
 const tripInfo = tripMain.querySelector(`.trip-info`);
 const tripControls = tripMain.querySelector(`.trip-controls`);
 const tripEvents = document.querySelector(`.trip-events`);
+const addEventButton = tripMain.querySelector(`.trip-main__event-add-btn`);
 
-render(tripInfo, new TripInfoComponent(cards).getElement(), RenderPosition.AFTERBEGIN);
 render(tripInfo, new CostComponent(cards).getElement(), RenderPosition.BEFOREEND);
 render(tripControls, new ControlsComponent().getElement(), RenderPosition.BEFOREEND);
 render(tripControls, new FiltersComponent(filters).getElement(), RenderPosition.BEFOREEND);
-render(tripEvents, new SortComponent(sortOptions).getElement(), RenderPosition.BEFOREEND);
 render(tripEvents, new EventContainerComponent().getElement(), RenderPosition.BEFOREEND);
 
 const tripDays = document.querySelector(`.trip-days`);
@@ -34,7 +33,7 @@ dates.forEach((day, index) => {
   render(tripDays, new DayComponent(day, index).getElement(), RenderPosition.BEFOREEND);
   const dayContainer = tripDays.querySelector(`.trip-days__item:last-of-type`);
   cards.filter((event) => day === new Date(event.startDate).toDateString())
-    .forEach((even) => {
+    .forEach((event) => {
       const eventList = dayContainer.querySelector(`.trip-events__list`);
 
       const onEscKeyDown = (evt) => {
@@ -53,14 +52,14 @@ dates.forEach((day, index) => {
         eventList.replaceChild(editEventComponent.getElement(), eventComponent.getElement());
       };
 
-      const eventComponent = new EventsComponent(even);
+      const eventComponent = new EventsComponent(event);
       const editButton = eventComponent.getElement().querySelector(`.event__rollup-btn`);
       editButton.addEventListener(`click`, () => {
         replaceEventToEdit();
         document.addEventListener(`keydown`, onEscKeyDown);
       });
 
-      const editEventComponent = new EditEventsComponent(even);
+      const editEventComponent = new EditEventsComponent(event);
       editEventComponent.getElement().addEventListener(`submit`, () => replaceEditToEvent);
 
       render(eventList, eventComponent.getElement(), RenderPosition.BEFOREEND);
@@ -68,3 +67,11 @@ dates.forEach((day, index) => {
 
     );
 });
+
+if (cards.length === 0) {
+  addEventButton.setAttribute(`disabled`, true);
+  render(tripEvents, new NoEventsComponent().getElement(), RenderPosition.BEFOREEND);
+} else {
+  render(tripInfo, new TripInfoComponent(cards).getElement(), RenderPosition.AFTERBEGIN);
+  render(tripEvents, new SortComponent(sortOptions).getElement(), RenderPosition.BEFOREEND);
+}
